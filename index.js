0465ad74b724748bdc7a8faa754d70cddc79509d
rafa <rafaelsistem87@gmail.com>
2021-03-31 23:51:19 -0700

    functions ready

---
+++ index.js
@@ -0,0 +1,191 @@
+const api = {
+      data : [{ "isActive": true,"content": "take out the trash"}],
+      
+      get: function () {
+       return new Promise((resolve) => {
+        setTimeout(() => {
+          console.log('data-get', this.data)
+          resolve(this.data)
+        }, 500)
+      })
+     },
+    
+     set: function () {
+        let item ={'isActive': true,'content': event.target.value}
+        this.data.push(item)
+        return new Promise((resolve) => {
+           setTimeout(() => {
+             console.log('data-set', this.data)
+             resolve(this.data)
+        }, 200)
+      })
+     },
+    
+     toggle: function () {
+       this.data.map( i => {
+         if( i.content === event.target.innerText ) {
+               i.isActive = (!i.isActive)
+           }
+        })
+          return new Promise((resolve) => {
+            setTimeout(() => {
+              console.log('data-toggle', this.data)
+              resolve(this.data)
+            }, 100)
+         })
+      },
+    
+     search: function (key, value) {
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            let search 
+            if ( typeof value === 'string') {
+              search = api.data.filter(i => i[key].indexOf(value) > -1 )
+            } else {
+              search = api.data.filter(i => i[key] === value )
+            }
+            resolve(search)
+          }, 50)
+        })
+      },
+    
+      delete: function () {
+        let clean = this.data.filter( bool => {
+          return bool.isActive === true;
+        })
+        
+        this.data = clean
+        return new Promise((resolve) => {
+           
+             console.log('data-delete', this.data)
+             resolve(this.data)
+        })
+     },
+    
+    }
+    
+    const all = document.getElementById("all");
+    all.addEventListener("click", function(){todoApp.filter()})
+    
+    const completed = document.getElementById("completed");
+    completed.addEventListener("click", function(){todoApp.filter('isActive', false)})
+    
+    const active = document.getElementById("active");
+    active.addEventListener("click", function(){todoApp.filter('isActive', true)})
+    
+    const remove = document.getElementById("remove");
+    remove.addEventListener("click", function(){todoApp.delete()}) 
+    
+    
+    class App {
+     constructor(lists, title, conteo, left) {
+        api.get().then( resolve => { 
+          this.data = resolve 
+          this.render()
+       })
+        this.lists = document.getElementById(lists)
+        this.title = title
+        this.conteo = document.getElementById(conteo)
+        this.left = document.getElementById(left)
+     }
+        
+      render () {
+        let count = this.data.length, listItems = " ";
+        while (count--){
+          
+         listItems += `<a 
+                          class="panel-block ${( this.data[count].isActive ? '' : 'is-active' )}" 
+                          onClick="todoApp.toggleState()">
+    
+                          <span class="panel-icon">
+                              <i class="fa fa-check"></i>
+                          </span>
+    
+                          ${this.data[count].content}
+                       </a>`
+        }
+    
+        const list = `<div class="panel bg-w">
+                        <p class="panel-heading">
+                         ${this.title}
+                        </p>
+    
+                        <input class="input" type="text"
+                               placeholder="Enter Text" 
+             
+                               onkeydown="javascript: if(event.keyCode == 13){
+                                    todoApp.add(this.value); 
+                                    this.value='';
+                               }">
+                       ${listItems}
+                      </div>`
+    
+          let completed = this.data.filter(i => i.isActive).length;
+          console.log('length',completed)
+    
+          
+          this.lists.innerHTML = list
+          this.conteo.innerHTML = `<h3>Tasks: ${this.data.length}</h3>` 
+          this.left.innerHTML = `<h3>Items Active: ${completed}</h3>`  
+         
+      }
+    
+    
+    
+     add(item){
+        api.set(item).then( resolve => {
+          this.data = resolve
+          this.render()  
+         
+          this.conteo.innerHTML = `<h3>Item Added</h3>`
+    
+          setTimeout(() => {
+              this.conteo.innerHTML = `<h3>Tasks: ${this.data.length}</h3>`
+           }, 3000)
+        })
+     }
+    
+     toggleState(completed) {
+        api.toggle().then( resolve => {
+        this.data = resolve
+        this.render()
+        
+      })
+      this.left.innerHTML = `<h3>Items Left: ${completed}</h3>` 
+     }
+     
+    
+     filter(key, val) {
+        if (key) {
+          api.search(key, val).then( resolve => {
+            this.data = resolve
+            this.render()
+          })
+        } else {  //All
+          api.get().then( resolve => {
+            this.data = resolve
+            this.render()
+          })
+        }
+      }
+      
+    
+      delete(clean,completed){
+        api.delete(clean).then( resolve => {
+          this.data = resolve
+          this.render()  
+        }) 
+        this.left.innerHTML = `<h3>Items Left: ${completed}</h3>` 
+    }
+    }
+    
+    
+    const todoApp = new App('lists', 'Tasks','conteo','left')
+    
+    
+    
+    
+    
+    
+    
+    
