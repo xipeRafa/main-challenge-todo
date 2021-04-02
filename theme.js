68033d2d7c07e9dd136af9eb28752a62e05fc7af
rafa <rafaelsistem87@gmail.com>
2021-04-01 20:42:51 -0700

    delete finder

--- theme.js
+++ theme.js
@@ -3,7 +3,7 @@
 let lsDarkMode=localStorage.getItem('dark-mode')
 const $dark_mode = document.getElementById("dark-mode-container"),
       darkModeActive=_=>{
-        $dark_mode.innerHTML=`<button id="dark-mode-active" class="dark-mode-btn">Ligth </button>`
+        $dark_mode.innerHTML=`<button id="dark-mode-active" class="dark-mode-btn">Light</button>`
         document.documentElement.style.setProperty('--first-color','#666')
         document.documentElement.style.setProperty('--second-color','#fff')
         document.documentElement.style.setProperty('--b-B-Color','#616061')
@@ -27,4 +27,39 @@ document.addEventListener("click",(e)=>{
   if(e.target.matches("#dark-mode-active")){
     darkModeDesActive()
   }
-}) 
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
