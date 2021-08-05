af313715dfb9e659eb6f4dfd881f8937d7230943
xipeRafa <43357824+xipeRafa@users.noreply.github.com>
2021-08-05 01:01:38 -0700

    new 2
    new 2

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
    ID("active").addEventListener("click", ()=> { todoApp.filter('isActive', true) })
 ID("completed").addEventListener("click", ()=> { todoApp.filter('isActive', false) })
 
+
 const api = {
 
     data: [{"isActive": true,"content": "bbbbbbb"},{"isActive": false,"content": "aaaaaaa"}],
@@ -10,31 +11,46 @@ const api = {
     get: function() { return new Promise((resolve) => { setTimeout(() => { resolve(this.data) }, 160) }) },
 
     set: function() {
-      this.data.unshift( {'isActive': true,'content': event.target.value} )
+      this.data.unshift({'isActive': true,'content': event.target.value} )
       return new Promise((resolve) => { setTimeout(() => { resolve(this.data) }, 150) })
     },
 
     toggle: function() {
-      this.data.map( i => {if(i.content === event.target.innerText){ i.isActive = (!i.isActive) }} )
+      this.data.map( el => {if(el.content === event.target.innerText){ el.isActive = (!el.isActive) }} )
       return new Promise((resolve) => { setTimeout(() => { resolve(this.data) }, 100) } ) 
     },
 
     search: function(key, value) {
       return new Promise((resolve) => {
-          typeof value === 'string' 
-             ? search = this.data.filter(i => i[key].indexOf(value) > -1) 
-             : search = this.data.filter(i => i[key] === value) 
+          typeof value === 'string' // or boolean
+             ? search = this.data.filter(el => el[key].indexOf(value) > -1, console.log('string'))  
+             : search = this.data.filter(el => el[key] === value, console.log('boolean')) 
           resolve(search)
       })
     },
 
     completed: function() {
-      this.data = this.data.filter( i => { return i.isActive === true })
+      this.data = this.data.filter( el => { return el.isActive === true })
       return new Promise((resolve) => { setTimeout(() => { resolve(this.data) }, 60) })
-    }
+    },
+
+    edited: function(){
+      let txt = event.target.previousElementSibling.previousElementSibling.innerText
+      ID('textId').value = txt
+      let ind = this.data.findIndex(el => el.content === txt)
+      this.data.splice(ind,1)  
+      return new Promise((resolve) => { setTimeout(() => { resolve(this.data) }, 100000) })  
+    },
+
+    deleted: function(){
+      let txt = event.target.previousElementSibling.innerText 
+      let ind = this.data.findIndex(el => el.content === txt)
+      this.data.splice(ind,1)
+      return new Promise((resolve) => { setTimeout(() => { resolve(this.data) }, 10) } )  
+    },
 }
 
- 
+
 class App {
   constructor(lists, left) {
     api.get().then(resolve => { this.data = resolve; this.render() }) 
@@ -49,12 +65,16 @@ class App {
         listItems += `
           <a class="panel-block ${( this.data[count].isActive ? '' : 'is-active' )}" onClick="todoApp.toggleState()">
             <span class="panel-icon">  <i class="fa fa-check"></i>  </span> ${this.data[count].content}
-          </a>`
-      }
+          </a> 
+          <div class="delet" onClick="todoApp.delet()">Delete</div>
+          <div class="edit" id='edit' onClick="todoApp.edit()">Edit</div> 
+        `
+      }  
       const list = `
         <input  type="text" 
                 class="input-text" 
                 placeholder="New todo..." 
+                id='textId'
                 onkeydown="javascript: if(event.keyCode == 13){todoApp.add(this.value); this.value = null }">
 
         <div class="list-tasks">${listItems}</div>`
@@ -63,17 +83,21 @@ class App {
       this.left.innerHTML = `<h3>Items Active: ${ this.data.filter(i => i.isActive).length }</h3>`
   }
     
-    add(item){ api.set(item).then(resolve => { this.data = resolve; this.render() }) }
+    add(value){ api.set(value).then(resolve => { this.data = resolve; this.render() }) }
 
     toggleState() { api.toggle().then(resolve => { this.data = resolve; this.render() }) }
 
     filter(key, val) { key ? api.search(key, val).then(resolve => { this.data = resolve; this.render() })
-                           : api.get().then(resolve => { this.data = resolve; this.render() }) }
+                           : api.get().then(resolve => { this.data = resolve; this.render() }) } // All
   
     cleanCompleted() { api.completed().then(resolve => { this.data = resolve; this.render() }) }
+
+    delet(){ api.deleted().then(resolve => { this.data = resolve; this.render() }) }
+    
+    edit(){ api.edited().then(resolve => { this.data = resolve; this.render() }) }
+
 }
 
 const todoApp = new App('lists', 'left') 
 
-/* .then(resolve => { this.data = resolve; this.render() })      update the info */
-/* return new Promise((resolve) => { setTimeout(() => { resolve(this.data) }, 150) })       (get)  update the info */
+
