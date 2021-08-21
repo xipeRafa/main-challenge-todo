ec60a331110c945dd11d8d503a48b30b6a4383d9
xipeRafa <43357824+xipeRafa@users.noreply.github.com>
2021-08-20 17:08:55 -0700

    fix edit 2
    fix edit 2

--- index.js
+++ index.js
@@ -6,15 +6,22 @@ ID("completed").addEventListener("click", ()=> { todoApp.filter('isActive', fals
 
 const api = {
 
-    data: [{'id': Date.now(),"isActive": true,"content": "aaaaaa"},
-           {'id': Date.now()+1,"isActive": false,"content": "bbbbbb"},
-           {'id': Date.now()+2,"isActive": false,"content": "cccccc"} ],
+    data: [
+           {id:Date.now()+1, isActive:true, content:"aaaaaa"},
+           {id:Date.now()+2, isActive:false, content:"bbbbbb"},
+           {id:Date.now()+3, isActive:true, content:"cccccc"}],
+
+    dataEdit:[{content:"edit mode"}],
 
     get: function() { return new Promise((resolve) => { setTimeout(() => { resolve(this.data) }, 160) }) },
 
     set: function() {
-      this.data.push({'id': Date.now(),'isActive': true,'content': event.target.value} )
-      return new Promise((resolve) => { console.log('data:', this.data), setTimeout(() => { resolve(this.data) }, 150) })
+      this.data.push({'id':Date.now(),'isActive': true,'content': event.target.value})
+
+     api.insert()
+
+     return new Promise((resolve) => { console.log('data:', this.data), setTimeout(() => { resolve(this.data) }, 150) })
+      
     },
 
     toggle: function() {
@@ -37,6 +44,8 @@ const api = {
     },
 
     edited: function(){
+      console.log('dataEdit:', this.dataEdit)
+
       /*let txt = event.target.previousElementSibling.previousElementSibling.id = Date.now() set id when clicked */ 
       let dis = document.getElementsByClassName("disabled")
       for (const el of dis) {
@@ -62,13 +71,41 @@ const api = {
       ID('textId').value = txt // move the text to input text
       ID('textId').style.border='2px solid lightblue'
       ID('textId').focus()
-      let ind = this.data.findIndex(el => el.id === idElementClicked)
-        console.log('index of Element clicked:', ind)
 
-      this.data.splice(ind,1)   
-       
-      return new Promise((resolve) => { setTimeout(() => { resolve(this.data) }, 100000) })   
+      let ind = this.data.findIndex(el => el.id === idElementClicked)
+      console.log('index of Element clicked:', ind)
+
+     
+
+      this.dataEdit.splice(0,1,{id:idElementClicked, index:ind, content:''})  
+
+      console.log('data:', this.data)
+      
     
+    },
+
+    insert: function(){
+
+      let newContent = this.data.slice(-1)[0].content // get the content to insert
+
+      let indexClicked = this.dataEdit.slice(0)[0].index //index of element clicked
+
+      let lengthClicked = this.dataEdit.slice(0)[0].content.length  //last element clicked (length)
+
+      let idClicked = this.dataEdit.slice(0)[0].id //id of element clicked
+        
+      if(lengthClicked===0){
+        console.log('if', lengthClicked)
+
+        this.data.splice(indexClicked,1,{'id': idClicked,"isActive": true,"content": newContent}) 
+
+        this.dataEdit.splice(0,1,{content:'edit mode again'})
+
+        this.data.pop()
+      }else{
+        console.log('else', lengthClicked)
+      }
+     
     },
 
     deleted: function(){
@@ -91,13 +128,16 @@ class App {
   render() {
       let count = this.data.length //index
       let listItems = " "
+       let l = this.data.map(el => el.content === 'To do List Ready to Use!!!' ? 'verr' : 'flases')[0]
+      
       while (count--) {
         listItems += `
-          <a class="panel-block disabled ${( this.data[count].isActive ? '' : 'is-active' )}" onClick="todoApp.toggle()">
+          <a class=" panel-block disabled ${ this.data[count].isActive ? '' : 'is-active' }" onClick="todoApp.toggle()">
             <span class="panel-icon">  <i class="fa fa-check"></i>  </span>  ${this.data[count].content}
           </a> 
-          <button class="delet disabled" onClick="todoApp.delet()">Delete</button>
+          <button class="delet disabled id="${l}" " onClick="todoApp.delet()">Delete</button>
           <button class="edit disabled"  onClick="todoApp.edit()">Edit</button> 
+         
         `
       }  
       const list = `
@@ -130,141 +170,9 @@ class App {
 
     delet(){ api.deleted().then(resolve => { this.data = resolve; this.render() }) }
     
-    edit(){ api.edited().then(resolve => { this.data = resolve; this.render() }) }
+    edit(){ api.edited() }
 
 }
 
 const todoApp = new App('lists', 'left') 
-
-
-
-
-
--------------------------------------------------------------------------------
-      
-      
-      
-      const mascotaInput = document.querySelector('#mascota');
-const contenedorCitas = document.querySelector('#citas');
-const formulario = document.querySelector('#nueva-cita')
-
-formulario.addEventListener('submit', nuevaCita); //82
-
-let editando = false;
-
-const citaObj = {
-    mascota: '',
-}
-
-eventListeners();
-function eventListeners() { mascotaInput.addEventListener('change', datosCita) }
-function datosCita(e) { citaObj[e.target.name] = e.target.value; console.log(e.target.name)}
-
-class Citas {
-    constructor() {
-        this.citas = []
-    }
-
-    agregarCita(cita) { this.citas = [...this.citas, cita] }
-
-    editarCita(update) { this.citas = this.citas.map( cita => cita.id === update.id ? update : cita)}
-
-    eliminarCita(id) { this.citas = this.citas.filter( cita => cita.id !== id) }
-}
-
-
-class UI {
-
-    imprimirAlerta(mensaje, tipo) {
-        const divMensaje = document.createElement('div');
-        divMensaje.classList.add('text-center', 'alert', 'd-block', 'col-12');
-        divMensaje.textContent = mensaje;
-
-        tipo === 'error' ? divMensaje.classList.add('alert-danger') : divMensaje.classList.add('alert-success')
-
-        document.querySelector('#contenido').insertBefore( divMensaje , document.querySelector('.agregar-cita'));
-
-        setTimeout( () => { divMensaje.remove()}, 3000)
-    }
-
-   imprimirCitas({citas}) { // Se puede aplicar destructuring desde la función...
-       
-        this.limpiarHTML();
-
-        citas.forEach(cita => {
-            const {mascota, id } = cita;
-
-            const divCita = document.createElement('div');
-            divCita.classList.add('cita', 'p-3');
-            divCita.dataset.id = id;
-
-            const mascotaParrafo = document.createElement('h2');
-            mascotaParrafo.classList.add('card-title', 'font-weight-bolder');
-            mascotaParrafo.innerHTML = `${mascota}`;
-
-            const btnEliminar = document.createElement('button');
-            btnEliminar.onclick = () => eliminarCita(id); // 108
-            btnEliminar.classList.add('btn', 'btn-danger', 'mr-2');
-            btnEliminar.innerHTML = 'Eliminar'
-
-            const btnEditar = document.createElement('button');
-            btnEditar.onclick = () => cargarEdicion(cita); //113
-            btnEditar.classList.add('btn', 'btn-info');
-            btnEditar.innerHTML = 'Editar'
-
-            divCita.appendChild(mascotaParrafo)
-            divCita.appendChild(btnEliminar)
-            divCita.appendChild(btnEditar)
-
-            contenedorCitas.appendChild(divCita);
-        })  
-   }
-
-   limpiarHTML() { while(contenedorCitas.firstChild) { contenedorCitas.removeChild(contenedorCitas.firstChild) } }
-}
-
-const ui = new UI();
-
-const instCitas = new Citas();
-
-function nuevaCita(e) {
-    e.preventDefault();
-
-    const {mascota} = citaObj;
-
-    if( mascota === '' ) { return ui.imprimirAlerta('Todos los mensajes son Obligatorios', 'error') }
-
-    if(editando) {
-        instCitas.editarCita( {...citaObj} );
-        ui.imprimirAlerta('Guardado Correctamente');
-        formulario.querySelector('button[type="submit"]').textContent = 'Crear Cita';
-        editando = false;
-    } else {
-        citaObj.id = Date.now();
-        instCitas.agregarCita({...citaObj});
-        ui.imprimirAlerta('Se agregó correctamente')
-    }
-
-    ui.imprimirCitas(instCitas);
-    reiniciarObjeto() //107
-    formulario.reset();
-}
-
-function reiniciarObjeto() { citaObj.mascota = '' }
-
-function eliminarCita(id) {
-    instCitas.eliminarCita(id);
-    ui.imprimirCitas(instCitas)
-}
-
-function cargarEdicion(cita) {
-    const {mascota, id } = cita;
-    citaObj.mascota = mascota;
-    citaObj.id = id;
-    mascotaInput.value = mascota;
-    mascotaInput.focus()
-    formulario.querySelector('button[type="submit"]').textContent = 'Guardar Cambios';
-
-    editando = true;
-}
 
